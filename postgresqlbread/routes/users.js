82a0ecdae47b0674a7c25e0095f9218cf3e25c00
sulthanrizal <surie0312@gmail.com>
2023-12-06 20:16:10 +0700

    Membuat Login

--- postgresqlbread/routes/users.js
+++ postgresqlbread/routes/users.js
@@ -2,11 +2,12 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 const moment = require('moment')
+var { isLoggedInd } = require('../helper/util.js')
 
 
 module.exports = function (db) {
 
-  router.get('/', async (req, res) => {
+  router.get('/', isLoggedInd, async (req, res) => {
     const { page = 1, title, startdate, enddate, complete, mode, sort = 'desc', sortby = 'id' } = req.query
     const params = []
     const queris = []
@@ -15,21 +16,31 @@ module.exports = function (db) {
       params.push(title)
       queris.push(` title ILIKE '%' ||$${params.length} || '%'`)
     }
-    db.query(`SELECT * FROM todos WHERE userid=${params.length}`, (err, { rows }) => {
+    params.push(req.session.user.userid)
+    db.query(`SELECT * FROM todos WHERE userid=${params.length}`, (err, { rows: data }) => {
       if (err) return res.send(err)
-      res.render('users/list', { data: rows, query: req.query, user: req.session.user, moment })
+      res.render('users/list', { data, query: req.query, page, moment, akun: akun[0], user: req.session.user.userid })
+      console.log(data)
     })
   })
 
-  router.get('/add', (req, res) => {
+  router.get('/add', isLoggedInd, (req, res) => {
     res.render('users/add')
   })
 
-  router.post('/add', (req, res) => {
+
+  router.post('/add', isLoggedInd, (req, res) => {
     db.query('INSERT INTO todos (title, userid) values ($1, $2)', [req.body.title, req.session.user.userid], (err) => {
-      console.log(req.session.user.userid)
       if (err) res.send(err)
       res.redirect('/users')
+    })
+  })
+
+  router.get('/edit/:id', isLoggedInd, (req, res) => {
+    const id = req.params.id
+    db.query('SELECT * FROM todos WHERE id = $1', [id], (err, { rows: data }) => {
+      if (err) return res.send(err)
+      res.render('users/edit', { data, moment })
     })
   })
 
